277b9bc2017d8d6c072c92762025d39c50164fef
GabrielGPTech <githubgpena.squire500@passinbox.com>
2025-02-22 22:03:25 -0300

    Add UserRepository interface for user data handling
    Introduce a UserRepository extending JpaRepository to manage User entities. Provides methods to find users by email and check if an email exists. This facilitates database operations related to user management.

---
+++ src/main/java/com/tattoo/api_tattoo/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tattoo.api_tattoo.domain.repository;
+
+import com.tattoo.api_tattoo.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+    Boolean existsByEmail(String email);
+}
